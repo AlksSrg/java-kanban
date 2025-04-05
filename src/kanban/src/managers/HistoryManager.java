6becd5f3cc53e4a259cf90b01abd52dcf4407fe9
Sergeev Aleksandr <frolvrn36@yandex.ru>
2025-04-05 03:47:06 +0300

    ФЗ 6
    + правки по CheckStyle

--- src/kanban/src/managers/HistoryManager.java
+++ src/kanban/src/managers/HistoryManager.java
@@ -4,7 +4,7 @@ import kanban.src.tasks.Task;
 
 import java.util.List;
 
-public interface HistoryManager {//общие вид методов класса HistoryManager
+public interface HistoryManager { //общие вид методов класса HistoryManager
 
     void add(Task task);//добавление в историю просмотров
 
